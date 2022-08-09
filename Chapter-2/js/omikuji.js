3a570155d69a9af771be2e9718c1962fedadbaea
sakiyamarui <litogaming1110@gmail.com>
2022-08-09 22:25:56 +0900

    [Upadate] Webアプリケーションを作ってみよう-2

---
+++ Chapter-2/js/omikuji.js
@@ -0,0 +1,25 @@
+let username;
+let userResult;
+
+username = prompt("お名前を教えてください。");
+document.getElementById('name').innerHTML = username;
+
+let rand = Math.floor(Math.random() * 5);
+
+if (rand == 0) {
+    userResult = "大吉";
+}
+if (rand == 1) {
+    userResult = "中吉";
+}
+if (rand == 2) {
+    userResult = "小吉";
+}
+if (rand == 3) {
+    userResult = "吉";
+}
+if (rand == 4) {
+    userResult = "凶";
+}
+
+document.getElementById('result').innerHTML = userResult;
